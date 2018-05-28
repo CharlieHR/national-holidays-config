3890163e6bf6df86ea636879254465256800e427
Alex Balhatchet <alex@balhatchet.net>
2018-05-28 14:07:40 +0100

    Simplify YAML format
    Instead of having an array of 'singleton' hashes, just have an array.
    
    Put all the years into a key 'years' so that the top level keys are
    always just 'name' and 'years'.

--- bin/extract_national_holidays_from_old_ruby_project.rb
+++ bin/extract_national_holidays_from_old_ruby_project.rb
@@ -21,20 +21,21 @@ Dir.chdir config_directory do
 
     Dir.chdir country_code do
       country_config.regions.each do |region_config|
-        config = { 'name' => region_config.region_name }
+        config = {
+          'name' => region_config.region_name,
+          'years' => {},
+        }
 
         region_config.regional_national_holidays.each do |national_holiday|
           year = national_holiday.start_date.year.to_s
-          config[year] ||= []
-          config[year] << {
-            national_holiday.english_name => {
-              'public_holiday' => true,
-              'start_date' => national_holiday.start_date.strftime('%Y-%m-%d'),
-              'end_date' => national_holiday.end_date.strftime('%Y-%m-%d'),
-              'names' => {
-                'en' => national_holiday.english_name,
-                local_language_code => national_holiday.local_name
-              }
+          config['years'][year] ||= []
+          config['years'][year] << {
+            'public_holiday' => true,
+            'start_date' => national_holiday.start_date.strftime('%Y-%m-%d'),
+            'end_date' => national_holiday.end_date.strftime('%Y-%m-%d'),
+            'names' => {
+              'en' => national_holiday.english_name,
+              local_language_code => national_holiday.local_name
             }
           }
         end
