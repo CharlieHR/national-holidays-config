df4ed6d9b11f76052989c166a51bed3d7ee3acad
Alex Balhatchet <alex@balhatchet.net>
2018-06-23 15:18:32 +0100

    Regions test - test for unique region names
    Region names must be unique even across countries.

---
+++ tests/regions_test.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require './tests/helper'
+
+describe 'Regions' do
+  describe 'must have unique codes across all countries' do
+    seen = {}
+
+    Dir.chdir(TestUtils.config_directory) do
+      Dir.glob('*/*.yml').each do |region_file|
+        region_name = region_file.sub(%r{^.+/}, '').sub(%r{\.yml$}, '')
+
+        it "region #{region_name} for #{region_file} is unique" do
+          if seen.key?(region_name)
+            flunk "already seen in #{seen[region_name]}!"
+          else
+            pass
+            seen[region_name] = region_file
+          end
+        end
+      end
+    end
+  end
+end
