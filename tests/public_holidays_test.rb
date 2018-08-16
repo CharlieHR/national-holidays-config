77977754b9ce0eabf88a4a18d142a823a7015423
Alex Balhatchet <alex@balhatchet.net>
2018-08-16 11:26:41 +0100

    There's no 25th month! Kenya public holidays bug fix
    Plus a test to catch this kind of error in the future :-)

--- tests/public_holidays_test.rb
+++ tests/public_holidays_test.rb
@@ -3,7 +3,32 @@
 require 'minitest/autorun'
 require './tests/helper'
 
+require 'date'
+
 describe 'Public Holidays' do
+  describe 'must have a valid date' do
+    Dir.chdir(TestUtils.config_directory) do
+      Dir.glob('*/*.yml').each do |region_file|
+        region_data = YAML.safe_load(File.read(region_file))
+
+        region_name = region_data['name']
+
+        region_data['years'].each do |year, holidays|
+          holidays.each do |holiday|
+            it "region #{region_name} holiday #{holiday['names'].values.first} has a valid date" do
+              date = begin
+                       Date.parse(holiday['date'])
+                     rescue ArgumentError
+                       nil
+                     end
+              date&.strftime('%Y-%m-%d').must_equal holiday['date']
+            end
+          end
+        end
+      end
+    end
+  end
+
   describe 'must occur within the year that they are under' do
     Dir.chdir(TestUtils.config_directory) do
       Dir.glob('*/*.yml').each do |region_file|
